61b269058acc1d35d157887b9f3041985ac34879
Franco Pérez <franlepeva@gmail.com>
2026-02-09 19:16:10 -0300

    script.js: Se añadieron funciones para analizar palabras. Función crearObjeto genera un objeto con la palabra y métodos para devolver largo e inicial, el cual se añade al arreglo de palabras, y es ejecutada al llamar a la función ingresarPalabra. La función mostrarArreglo muestra una lista de las palabras ingresadas hasta el momento, iterando sobre el arreglo de objetos. La función filtrarInicial retorna un mensaje con las palabras cuya letra inicial coincide con la seleccionada. Las funciones filtrarMayores y filtrarMenores devuelven un mensaje enumerando las palabras con igual o más, o igual o menos letras que el número seleccionado, respectivamente. Estas funciones se llaman desde la estructura condicional correspondiente al programa de análisis de palabras en la función programas, mostrando los mensajes con alert.

--- script.js
+++ script.js
@@ -19,6 +19,7 @@ function iniciar() {
             break;
         }
         else {
+            alert("Por favor ingrese una alternativa válida");
             console.log("Por favor ingrese una alternativa válida");
             inicio = 0;
         }
@@ -60,11 +61,45 @@ function programas(inicio) {
             break;
         case 2:
             // Programa de palabras
-            alert("Programa de Análisis de Palabras");
             console.log("Programa de palabras");
 
-            // Llamamos a función para ingresar palabra
-            ingresarPalabra();
+            let altern = parseInt(prompt("Programa de Análisis de Palabras\nElija una opción:\n1. Ingresar nueva palabra\n2.Mostrar palabras\n3.Filtrar palabras por inicial\n4.Filtrar palabras por largo mayor a...\n5.Filtrar palabras por largo menor a..."));
+            if (altern == 1) {
+                // Llamamos a función para ingresar palabra
+                ingresarPalabra();
+            } else if (altern == 2) {
+                if (palabras.length == 0) {
+                    alert("No hay palabras guardadas");
+                    ingresarPalabra();
+                } else {
+                    mostrarArreglo();
+                }
+            } else if (altern == 3) {
+                if (palabras.length == 0) {
+                    alert("No hay palabras guardadas");
+                    ingresarPalabra();
+                } else {
+                    let ini = prompt("Ingrese letra");
+                    alert(filtrarInicial(ini));
+                }
+            } else if (altern == 4) {
+                if (palabras.length == 0) {
+                    alert("No hay palabras guardadas");
+                    ingresarPalabra();
+                } else {
+                    let mayorA = prompt("Ingrese número");
+                    alert(filtrarMayores(mayorA));
+                }
+            } else if (altern == 5) {
+                if (palabras.length == 0) {
+                    alert("No hay palabras guardadas");
+                    ingresarPalabra();
+                } else {
+                    let menorA = prompt("Ingrese número");
+                    alert(filtrarMenores(menorA));
+                }
+            }
+            
             break;
     }
     // Nos deshacemos del ciclo while innecesario llamando nuevamente a la función iniciar
@@ -106,8 +141,75 @@ function ingresarPalabra() {
         console.log(parseInt(palabra));
         palabra = prompt("Por favor, ingrese sólo palabras:");
     }
-    palabras.push(palabra);
+    palabras.push(crearObjeto(palabra));
     console.log(palabras);
-    let cant = palabra.length;
-    alert("Usted ingresó la palabra: "+palabra+"\nSu palabra tiene "+cant+" letras");
+    alert("Usted ingresó la palabra: " + palabra + "\nSu palabra tiene " + palabra.length + " letras");
+}
+
+function crearObjeto(pal) {
+    let objeto = {
+        palabra: pal,
+        retornarLargo: function() {
+            return this.palabra.length;
+        },
+        retornarInicial: function() {
+            return this.palabra[0];
+        }
+    }
+    return objeto;
+}
+
+function mostrarArreglo() {
+    let msj = "";
+    console.log(msj);
+    for (let i = 0; i < palabras.length; i++) {
+        console.log(palabras[i]);
+        let ord = i+1;
+        msj += "La palabra " + ord + " es : " + palabras[i].palabra + "\n";    
+        console.log(msj);
+    }
+    console.log(msj);
+    alert(msj);
+}
+
+function filtrarInicial(ini) {
+    let filtrado = "";
+    let contar = 0;
+    let msj = "";
+    palabras.forEach(pal => {
+        if (pal.retornarInicial() == ini) {
+            filtrado += (pal.palabra) + "\n";
+            contar += 1;
+        }
+    })
+    msj = "Se encontraron " + contar + " palabras que comienzan con " + ini + ":\n" + filtrado;
+    return msj;
+}
+
+function filtrarMayores(num) {
+    let filtrado = "";
+    let contar = 0;
+    let msj = "";
+    palabras.forEach(pal => {
+        if (pal.retornarLargo() >= num) {
+            filtrado += (pal.palabra) + "\n";
+            contar += 1;
+        }
+    })
+    msj = "Se encontraron " + contar + " palabras con " + num + " letras o más:\n" + filtrado;
+    return msj;
+}
+
+function filtrarMenores(num) {
+    let filtrado = "";
+    let contar = 0;
+    let msj = "";
+    palabras.forEach(pal => {
+        if (pal.retornarLargo() <= num) {
+            filtrado += (pal.palabra) + "\n";
+            contar += 1;
+        }
+    })
+    msj = "Se encontraron " + contar + " palabras con " + num + " letras o menos:\n" + filtrado;
+    return msj;
 }
